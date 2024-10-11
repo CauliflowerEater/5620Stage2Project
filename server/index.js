89d1fe3a553bcd20698c34e3b46ab5b929cf5529
Shawn Li <shli0267@uni.sydney.edu.au>
2024-10-11 22:47:02 +1100

    initialized the project

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from Express!" });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
