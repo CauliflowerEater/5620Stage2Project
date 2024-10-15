b5433fa1aa14e6c1fde647255eccfc0a3dd15426
Shawn Li <shli0267@uni.sydney.edu.au>
2024-10-16 01:22:37 +1100

    Finish login signup API

--- server/App.js
+++ server/App.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/config"); // 连接数据库的代码
+const userRoutes = require("./routes/userRoutes"); // 引入路由
+require("module-alias/register"); //快捷定义引用目录
+
+dotenv.config();
+
+// 创建Express应用
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from Express!" });
-});
+// 连接数据库
+connectDB();
 
-app.listen(port, () => {
-  console.log(`Server running on port http://localhost:${port}`);
+// 中间件
+app.use(express.json()); // 解析JSON请求
+
+// 使用路由
+app.use("/api", userRoutes);
+
+// 启动服务器
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
---
+++ server/controllers/userController.js
@@ -0,0 +1,24 @@
+const loginService = require("../services/userServices/loginService");
+const registerService = require("../services/userServices/registerService");
+
+const loginUser = async (req, res) => {
+  const { userName, password } = req.body;
+  try {
+    await loginService(userName, password);
+    res.status(200).json({ message: "Login Successsful" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const registerUser = async (req, res) => {
+  const { userName, password, email } = req.body;
+  try {
+    await registerService(userName, password, email);
+    res.status(200).json({ message: "Account registered successfully" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = { loginUser, registerUser };
