ce36521c16ca9dafc68cb0674902d7c45e1d40a2
Yonghao Lyu <snake_lvyonghao@163.com>
2024-10-23 02:45:59 +1100

    delete log

--- server/controllers/conditionReportController.js
+++ server/controllers/conditionReportController.js
@@ -26,7 +26,6 @@ const getConditionReportsController = async (req, res) => {
 
     try {
         const reports = await getConditionReports(userId);
-        console.log(reports)
         res.status(200).json(reports);
     } catch (error) {
         res.status(400).json({ message: error.message });
